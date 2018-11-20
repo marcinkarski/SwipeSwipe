c8896ef5d17e971061554d9dff4c121cdd694455
Marcin Karski <karski80@gmail.com>
2018-11-20 17:10:58 +0000

    Basic stack view setup

--- SwipeSwipe/AppDelegate.swift
+++ SwipeSwipe/AppDelegate.swift
@@ -9,10 +9,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let viewController = ViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        
+
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = navigationController
+        window?.rootViewController = viewController
         window?.makeKeyAndVisible()
         
         return true
--- SwipeSwipe/ViewController.swift
+++ SwipeSwipe/ViewController.swift
@@ -9,5 +9,15 @@ class ViewController: UIViewController {
     
     private func setup() {
         view.backgroundColor = .white
+        let redView = UIView()
+        redView.backgroundColor = .red
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let stackView = UIStackView(arrangedSubviews: [redView, blueView])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor), stackView.topAnchor.constraint(equalTo: view.topAnchor), stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor), stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
     }
 }
