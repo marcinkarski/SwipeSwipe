78e5344c1ca21802fbf405aedbfb2b363808ab3f
Marcin Karski <karski80@gmail.com>
2018-11-21 21:36:37 +0000

    Image to be fixed

--- SwipeSwipe/Controllers/ViewController.swift
+++ SwipeSwipe/Controllers/ViewController.swift
@@ -13,14 +13,15 @@ class ViewController: UIViewController {
     }
     
     private func dummyCard() {
-        
-        
+        let cardView = CardView(frame: .zero)
+//        cardView.contentMode = .scaleAspectFill
+//        cardView.clipsToBounds = true
+        cardDeckView.addSubview(cardView)
+        NSLayoutConstraint.activate([cardView.leadingAnchor.constraint(equalTo: cardDeckView.leadingAnchor), cardView.topAnchor.constraint(equalTo: cardDeckView.topAnchor), cardView.trailingAnchor.constraint(equalTo: cardDeckView.trailingAnchor), cardView.bottomAnchor.constraint(equalTo: cardDeckView.bottomAnchor)])
     }
     
     private func setup() {
         view.backgroundColor = .white
-
-        cardDeckView.backgroundColor = .yellow
         
         let stackView = UIStackView(arrangedSubviews: [topStackView, cardDeckView, bottomStackView])
         stackView.axis = .vertical
@@ -28,5 +29,7 @@ class ViewController: UIViewController {
         
         view.addSubview(stackView)
         NSLayoutConstraint.activate([stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor), stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor), stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor), stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)])
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = .init(top: 0, left: 8, bottom: 0, right: 8)
     }
 }
--- SwipeSwipe/Views/CardView.swift
+++ SwipeSwipe/Views/CardView.swift
@@ -1,9 +1,18 @@
 import UIKit
 
 class CardView: UIView {
+    
+    private let image = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor = .red
+        layer.cornerRadius = 12
+        layer.masksToBounds = true
+        image.contentMode = .bottom
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(image)
+//        NSLayoutConstraint.activate([image.leadingAnchor.constraint(equalTo: self.leadingAnchor), image.topAnchor.constraint(equalTo: self.topAnchor), image.trailingAnchor.constraint(equalTo: self.trailingAnchor), image.bottomAnchor.constraint(equalTo: self.bottomAnchor)])
     }
     
     required init?(coder aDecoder: NSCoder) {
