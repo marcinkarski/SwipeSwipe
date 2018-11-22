9ec2b274785c3c047819eeed98909f7a8b26a997
Marcin Karski <karski80@gmail.com>
2018-11-22 22:04:05 +0000

    Pan Gesture implemented

--- SwipeSwipe/Controllers/ViewController.swift
+++ SwipeSwipe/Controllers/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController {
     
     private func dummyCard() {
         let cardView = CardView(frame: .zero)
-//        cardView.contentMode = .scaleAspectFill
-//        cardView.clipsToBounds = true
         cardDeckView.addSubview(cardView)
         NSLayoutConstraint.activate([cardView.leadingAnchor.constraint(equalTo: cardDeckView.leadingAnchor), cardView.topAnchor.constraint(equalTo: cardDeckView.topAnchor), cardView.trailingAnchor.constraint(equalTo: cardDeckView.trailingAnchor), cardView.bottomAnchor.constraint(equalTo: cardDeckView.bottomAnchor)])
     }
@@ -31,5 +29,7 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate([stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor), stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor), stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor), stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)])
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.layoutMargins = .init(top: 0, left: 8, bottom: 0, right: 8)
+        
+        stackView.bringSubviewToFront(cardDeckView)
     }
 }
--- SwipeSwipe/Views/CardView.swift
+++ SwipeSwipe/Views/CardView.swift
@@ -18,9 +18,21 @@ class CardView: UIView {
     }
     
     @objc private func handlePan(gesture: UIPanGestureRecognizer) {
-        let translation = gesture.translation(in: self)
-        print(translation.x)
-        transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+
+        
+        switch gesture.state {
+        case .changed:
+            let translation = gesture.translation(in: self)
+            transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        case .ended:
+            UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+                self.transform = .identity
+            }) { (_) in
+                
+            }
+        default:
+            break
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
