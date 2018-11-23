34c4da1cf82f0eb386a5fb346b6279e4f261ea75
Marcin Karski <karski80@gmail.com>
2018-11-23 14:13:49 +0000

    Implemented rotation of card

--- SwipeSwipe/Views/CardView.swift
+++ SwipeSwipe/Views/CardView.swift
@@ -3,6 +3,7 @@ import UIKit
 class CardView: UIView {
     
     private let image = UIImageView(image: #imageLiteral(resourceName: "img1"))
+    private let treshold: CGFloat = 100
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,12 +23,21 @@ class CardView: UIView {
         switch gesture.state {
         case .changed:
             let translation = gesture.translation(in: self)
-            transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+            let degrees: CGFloat = translation.x / 20
+            let angle = degrees * .pi / 180
+            let rotation = CGAffineTransform(rotationAngle: angle)
+            transform = rotation.translatedBy(x: translation.x, y: translation.y)
         case .ended:
+            let shouldDismissCard = gesture.translation(in: self).x > treshold
             UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
-                self.transform = .identity
+                if shouldDismissCard {
+                    self.frame = CGRect(x: 1000, y: 0, width: self.frame.width, height: self.frame.height)
+                } else {
+                    self.transform = .identity
+                }
             }) { (_) in
-                
+                self.transform = .identity
+                self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
             }
         default:
             break
